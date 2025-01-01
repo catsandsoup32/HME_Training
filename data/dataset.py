f7d6b0cbb1f6a15d3765bb1eb065c90c58f9cd12
Edmund Bu <2edmundbu@gmail.com>
2024-12-31 23:18:09 -0800

    before big training

--- data/dataset.py
+++ data/dataset.py
@@ -1,48 +1,48 @@
 import torch
-from torch.utils.data import Dataset, DataLoader
+import torch.nn as nn
+from torch.utils.data import Dataset
+from torchvision.datasets import ImageFolder
 from torch import Tensor
 from PIL import Image
 import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from tokenizer import LaTeXTokenizer
-from pathlib import Path
-
-
-data_path = Path(r'data\mathwriting_2024')
-cache_path = Path(r'data\full_cache')
-
-tokenizer = LaTeXTokenizer()
-latexList = []
-for latex_file in cache_path.glob("valid/*.txt"):
-    with open(latex_file) as f:
-            latexList.append(str(f.read()))
 
 
 class MathWritingDataset(Dataset): 
-    def __init__(self, data_dir, cache_dir, mode=None, transform=None): # Note that data_dir is from the original data, not cache
+    def __init__(self, data_dir, cache_dir, mode=None, transform=None, tokenizer=None): # Note that data_dir is from the original data and maps to the cache
         self.data_dir = data_dir
         self.cache_dir = cache_dir
         self.mode = mode
         self.transform = transform
-        tokenizer.build_vocab(latexList) # Takes list as input to assign IDs
+        self.tokenizer = tokenizer
+        self.train_length = len(os.listdir(os.path.join(self.data_dir, 'train')))
+        self.synthetic_length = len(os.listdir(os.path.join(self.data_dir, 'synthetic')))
 
     def __len__(self):
         if self.mode == 'train':
-            return len(os.listdir(os.path.join(self.data_dir, 'train')))
+            return self.train_length
         elif self.mode == 'valid':
             return len(os.listdir(os.path.join(self.data_dir, 'valid')))
         elif self.mode == 'test':
             return len(os.listdir(os.path.join(self.data_dir, 'test')))
+        elif self.mode == 'train+synthetic':
+            return self.train_length + self.synthetic_length
+        else:
+            print("Typo in mode parameter")
     
     def __getitem__(self, idx):
-        fileID = str( os.listdir(os.path.join(self.data_dir, self.mode))[idx] ).removesuffix('.inkml')[-16:] 
+        if self.mode != 'train+synthetic':
+            fileID = str(os.listdir(os.path.join(self.data_dir, self.mode))[idx]).removesuffix('.inkml')[-16:] # Last 16 chars is the unique ID
+        else:
+            if idx < self.train_length: 
+                fileID = str(os.listdir(os.path.join(self.data_dir, 'train'))[idx]).removesuffix('.inkml')[-16:]
+            else: 
+                fileID = str(os.listdir(os.path.join(self.data_dir, 'synthetic'))[idx-self.train_length]).removesuffix('.inkml')[-16:] # (idx - train_length) to start from 0
+
         image = Image.open(os.path.join(self.cache_dir, self.mode, fileID + '.png'))
-        
         latex = open(os.path.join(self.cache_dir, self.mode, fileID + '.txt')).read()
-        label = tokenizer.encode(latex)
+        label = self.tokenizer.encode(latex)
         label = Tensor(label)
-        reversed_label = torch.flip(label, dims=[0])    
+        reversed_label = torch.flip(label)    
 
         return self.transform(image), label, reversed_label 
     
